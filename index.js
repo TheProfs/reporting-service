beee4aba3d6953ffbf7a9f321407b3c8e1efa353
nicholaswmin <nik.kyriakides@gmail.com>
2018-03-23 09:58:08 +0200

    added server deps and wrote an API test

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '10mb' }))
+app.set('port', (process.env.PORT || 5004))
+
+// body
+
+
+// end body
+
+app.listen(app.get('port'), () => {
+  console.info(`Success! Listening on: ${app.get('port')}`)
+})
+
+module.exports = app
--- test/index.js
+++ test/index.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../index.js')
+
+chai.should()
+chai.use(chaiHttp)
+
+describe('POST: /report/bug', () => {
+  it('sends JSON to a Slack Channel & responds with HTTP 204 status', () => {
+    return chai.request(app)
+      .put('/report/bug')
+      .send({
+        foo: 'hello',
+        bar: 'world'
+      })
+      .then(res => {
+        res.should.have.status(204)
+      })
+      .catch(err => {
+        throw err
+      })
+  })
+})
